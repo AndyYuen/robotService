c5f5f0794b9ad1fe48e8861f9e87ac33867a94ac
ayuen <ayuen@redhat.com>
2020-01-28 09:01:20 +1100

    Updated to support zumo robot

--- src/main/java/com/redhat/demo/robotservice/rest/CommandEndpoint.java
+++ src/main/java/com/redhat/demo/robotservice/rest/CommandEndpoint.java
@@ -1,21 +1,25 @@
 package com.redhat.demo.robotservice.rest;
 
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.List;
 import java.util.Map;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthenticationException;
-
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -43,15 +47,15 @@ public class CommandEndpoint {
 		private static final long serialVersionUID = 1L;
 
 		{
-			put("LEADER", 		"192.168.1.126");
+			put("LEADER", 		"192.168.99.104");
 			put("FOLLOWER", 	"192.168.99.105");
-			put("DM_ROBOT", 	"192.168.99.103");
-			//put("DM_ROBOT", 	"192.168.2.112");
-			put("DANCE_ROBOT", 	"192.168.99.106");
-			put("OPENSHIFT", 	"192.168.99.104");
-			put("BUILDAH", 		"192.168.99.105");
-			put("PODMAN", 		"192.168.99.103");
-			put("CRI-O", 		"192.168.99.106");
+			//put("DM_ROBOT", 	"192.168.99.103");
+			put("DM_ROBOT", 	"192.168.0.11");
+			put("DANCE_ROBOT", 	"192.168.0.10");
+			put("OPENSHIFT", 	"192.168.0.12");
+			put("BUILDAH", 		"192.168.0.13");
+			put("PODMAN", 		"192.168.0.11");
+			put("CRI-O", 		"192.168.0.10");
 		}
 	};
 
@@ -75,23 +79,20 @@ public class CommandEndpoint {
 	public static void sendToRobot(String robot, String cmd, String ip) throws IOException, AuthenticationException, JSONException {
 		System.out.println("Robot command: " + String.format("{ \"cmd\" : \"%s;\" }", cmd));
 		DefaultHttpClient httpClient = new DefaultHttpClient();
-		HttpGet getRequest = new HttpGet(URLEncoder.encode(String.format("http://%s/exec?param=%s", ip, cmd), 
-				StandardCharsets.UTF_8.toString()));
-		getRequest.addHeader("accept", "application/json");
+		HttpPost postRequest = new HttpPost(String.format("http://%s/rpc/Robot.Cmd", ip));
+		StringEntity params =new StringEntity(String.format("{ \"cmd\" : \"%s;\" }", cmd));
+		postRequest.addHeader("accept", "application/json");
+		postRequest.addHeader("content-type", "application/json");
+		postRequest.setEntity(params);
+/*	    UsernamePasswordCredentials creds
+	      = new UsernamePasswordCredentials("user", "password");
+	    postRequest.addHeader(new BasicScheme().authenticate(creds, postRequest, null));*/
 
-
-		HttpResponse response = httpClient.execute(getRequest);
+		HttpResponse response = httpClient.execute(postRequest);
 
 		if (response.getStatusLine().getStatusCode() != 200) {
 			throw new RuntimeException("Failed : HTTP error code : "
 			   + response.getStatusLine().getStatusCode());
-		}
-		else {
-			String content = IOUtils.toString(response.getEntity().getContent(), "UTF-8");
-			httpClient.getConnectionManager().shutdown();
-
-			System.out.println("***********");
-			System.out.println(content);
 		}
 	}
 }
